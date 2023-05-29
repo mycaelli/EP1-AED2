a0693cb5a4f8e5617a6dd5e92385ffcf5c7c689b
mycaelli <mycaelli@usp.br>
2023-05-29 19:14:41 -0300

    dijkstra basico functionando

--- ep1.c
+++ ep1.c
@@ -4,8 +4,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
 #include <limits.h> /*pode incluir?*/
+#include <stdbool.h>
 
 // lib de bool
 
@@ -30,6 +30,7 @@ typedef struct estr
     struct estr *prox;
 } NO;
 
+
 // vertices do grafo (salas) - use este tipo ao criar o grafo  - NAO ALTERE ESTA DEFINICAO
 typedef struct 
 {
@@ -46,13 +47,20 @@ typedef struct
 	define se o vertice esta aberto ou fechado a partir do array estaAberto
 */
 VERTICE *criaVetices(int N, int *estaAberto) {
-	VERTICE *g = (VERTICE*) malloc(sizeof(VERTICE) * N);
-	for (int i = 0; i < N; i++) {
+	VERTICE *g = (VERTICE*) malloc(sizeof(VERTICE) * (N + 1));
+	for (int i = 1; i <= N; i++) {
 		g[i].flag = 0;
-		g[i].via = 0;
+		g[i].via = -1;
 		g[i].dist = INT_MAX;
 		g[i].inicio = NULL;
-		aberto[i] = estaAberto[i];
+		g[i].aberto = estaAberto[i];
+		// printf("%d %d %d %d %p %d\n", 
+		// i,		
+		// g[i].flag,
+		// g[i].via,
+		// g[i].dist,
+		// g[i].inicio,
+		// g[i].aberto);
 	}
 	return g;
 }
@@ -71,27 +79,33 @@ void insereArestas(VERTICE *g, int atual, int *ijpeso) {
 	int peso = ijpeso[atual*3+2];
 	
 	NO *novoNo = (NO*)malloc(sizeof(NO));
+	if (!novoNo) {
+		printf("erro de alocacao");
+	}
 	novoNo->adj = destino;
 	novoNo->peso = peso;
 	novoNo->prox = g[origem].inicio;
 	g[origem].inicio = novoNo;
 }
 
-/*
-	N = Grafo de N vertices
-	A = Grafo de A arestas
-	*ijpeso = Representa as arestas (tamanho 3 * A)
-		ex: A = 2, ijpeso={1,2,3,4,5,6}
-			grafo g possui uma aresta entre os vertices 1 e 2 com peso 3
-			e possui uma aresta entre os vertices 4 e 5 com peso 6
-	*aberto = indica se os vertices estao abertos ou fechados
-		- contem apenas valores 0 ou 1
-	inicio, fim = busca
-	chave = vertice chave
-*/
 // funcao principal
-NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int chave);
+//NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int chave);
 int encontraMenorDistancia(VERTICE *g, int N);
+NO* dijkstra(VERTICE *g, int origem, int destino,  int N, int *pesoTotal);
+void adicionaNosResposta(NO *resp, int adj);
+
+
+void imprimeGrafo(VERTICE *g, int N) {
+	for (int i = 1; i <= N; i++) {
+		printf("%d", i);
+		NO* p = g[i].inicio;
+		while(p) {
+			printf("->%d (%d) ", p->adj, p->peso);
+			p = p->prox;
+		}
+		printf("\n");
+	}
+}
 
 //------------------------------------------
 // O EP consiste em implementar esta funcao
@@ -102,74 +116,100 @@ NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int cha
 {
 	NO* resp;
 	resp = NULL;
+	int value = 0;
+	int *pesoSemChave = &value;
 	bool temChave = false;
 
 	// seu codigo AQUI
 
-	/*
-		inicializa grafo
-	*/
 	VERTICE* g = criaVetices(N, aberto);
 	for (int i = 0; i < A; i++) {
-		insereArestas(g, i, ijpeso)
+		insereArestas(g, i, ijpeso);
 	}
-	//...
+	imprimeGrafo(g, N);
+	resp = dijkstra(g, inicio, fim, N, pesoSemChave);
+	printf("pesoTotal %d\n", *pesoSemChave);
 
-	/*dist da origem pra ela mesma*/
-	g[inicio].dist = 0;
-
-	for (int i = 0; i < N; i++) {
-		/*encontra o vertice nao visitado com menor dist*/
-		int u = encontraMenorDistancia(g, N);
-
-		/*
-			se o vertice onde vai fazer a busca nao estiver aberto, pula ele
-		*/
-		if (g[u].aberto && temChave == false) {
-			temChave = true;
-		} else if (!g[u].aberto && temChave == false){
-			continue;
-		} 
-		/* marca o vertice atual como visitado */
-		g[u].flag = 1;
-		
-		NO* p = grafo[u].inicio;
-		while(p) {
-			int adj = p->adj;
-			int peso = p->peso;
-
-			/*
-				se
-					vertice atual nao foi visitado
-					dist do vertice atual nao é infinito -> evita erro de overflow
-					dist do vertice atual é menor que dist do vertice adj
-
-						dist do vertice adj é a dist do vertice atual + peso do vertice adj
-			*/
-			if (!g[adj].flag && (g[u].dist != INT_MAX) && (g[u].dist + peso < g[adj].dist)) {
-				g[adj].dist = g[u].dist + peso;
-				/* vai salvando em uma lista pra depois retornar? */
-			}
-			p = p->prox;
-		}
+	NO *atual = resp;
+	while(atual) { 
+		printf("%d->", atual->adj);
+		atual = atual->prox;
 	}
+
+	/* TEM QUE DAR FREE NO GRAFO */
+
 	return resp;
 }
 
+void adicionaNoInicio(NO** resp, int adj) {
+	NO *novoNo = (NO *)malloc(sizeof(NO));
+    novoNo->adj = adj;
+    novoNo->prox = *resp;
+    *resp = novoNo;
+}
+
 /*
-	encontra o vertice nao visitado com menor dist 
+	encontra o vertice com dist minima
 */
+
 int encontraMenorDistancia(VERTICE *g, int N) {
 	int min = INT_MAX;
-	int min_idx;
+	int min_idx = -1;
 
-	for (int i = 0; i < N; i++) {
-		if (g[i].flag == 0 && g[i].dist <= min) {
+	for (int i = 1; i <= N; i++) {
+		if (!g[i].flag && g[i].dist <= min) {
 			min = g[i].dist;
 			min_idx = i;
 		}
 	}
 	return min_idx;
+}
+
+NO* dijkstra(VERTICE *g, int origem, int destino,  int N, int *pesoTotal) {
+	g[origem].dist = 0;
+	g[origem].via = origem;
+
+	for (int i = 1; i <= N; i++) {
+		// if (!g[i].aberto) 
+		// 	continue;
+		int u = encontraMenorDistancia(g, N);
+		g[u].flag = 1;
+		NO* p = g[u].inicio;
+		while (p) {
+			int adj = p->adj;
+			int peso = p->peso;
+			if (!g[adj].flag && g[u].dist != INT_MAX && g[u].dist + peso < g[adj].dist) {
+				g[adj].dist = g[u].dist + peso;
+				g[adj].via = u;
+			}
+			p = p->prox;
+		}
+	}
+
+	// Recuperar o caminho mais curto
+    if (g[destino].dist == INT_MAX) {
+        printf("Nao ha caminho entre a origem e o destino.\n");
+        return NULL;
+    }
+
+	NO *resp =  NULL;
+	int atual = destino;
+
+	while (atual != origem) {
+    	int via = g[atual].via;
+		adicionaNoInicio(&resp, atual);
+    	NO* p = g[via].inicio;
+    	while (p) {
+    	    if (p->adj == atual) {
+    	        *pesoTotal += p->peso;
+    	        break;
+    	    }
+    	    p = p->prox;
+    	}
+    	atual = via;
+    }
+	adicionaNoInicio(&resp, atual);
+	return resp;
 }
 
 
@@ -179,21 +219,50 @@ int encontraMenorDistancia(VERTICE *g, int N) {
 int main() {
 
 
+
 	// aqui vc pode incluir codigo de teste
 
 	// exemplo de teste trivial
 
-	int N=3; // grafo de 3 vÈrtices numerados de 1..3
-	int aberto[] = {1,1,1}; // todos abertos
-	int inicio=1;
-	int fim=3;
-	int chave=2;
-	int ijpeso[]={1,2,10, 2,3,20, 3,1,10};
+	// int N=4; // grafo de 3 vÈrtices numerados de 1..3
+	// int aberto[] = {1,1,1,1}; // todos abertos
+	// int inicio=1;
+	// int fim=4;
+	// int chave=2;
+    // int A = 5;
+	// int ijpeso[]={1,2,10, 2,3,20, 3,1,10, 3,4,1, 1,4,3};
+
+    int N = 6;
+    int A = 8;
+    int aberto[] = {1, 1, 1, 1, 1, 0};
+    int inicio = 1;
+    int fim = 5;
+    int chave = 2;
+    int ijpeso[] = {
+        1, 2, 14,
+        1, 6, 3,
+        1, 4, 5,
+        2, 3, 6,
+        2, 6, 5,
+        3, 4, 8,
+        4, 5, 10,
+        5, 6, 8};
 
 	// o EP sera testado com uma serie de chamadas como esta
-	NO* teste = null;
-	teste = caminho(N, ijpeso, aberto, inicio, fim, chave);
-	return teste;
+	//NO* teste = NULL;
+	NO *teste;
+	teste = caminho(N, A, ijpeso, aberto, inicio, fim, chave);
+
+	// NO* ini = teste;
+	// while (ini) {
+	// 	printf("%d\n", ini->adj);
+	// 	ini = ini->prox;
+	// }
+	// for (int i = 0; i < N; i++) {
+	// 	printf("%d ", teste[i]);
+	// }
+	// printf("\n");
+	return 0;
 
 }
 
